c2835f455075225521710ca493d9dd787a71678c
ThxSitdown <surajakzaza15251@gmail.com>
2024-08-09 11:29:59 +0700

    .

--- src/signup/page.js
+++ src/signup/page.js
@@ -14,7 +14,7 @@ export default function Page() {
     const res = await fetch('http://localhost:3000/api/users', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        Accept : 'application/json',
       },
       body: JSON.stringify({ firstname, lastname, username, password }),
     });
